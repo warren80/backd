ca7b23499abcb49d8d8a8cacd8643becf85f2ac4
root <root@DataComm.(none)>
2012-06-28 10:37:01 -0700

    dunno

--- packetizer.rb
+++ packetizer.rb
@@ -137,7 +137,7 @@ class Connector
 
   def servRecv(pkt)
     a = pkt.ip_saddr.split(".")
-    return a[0].chr + a[1].chr + a[2].chr + a[3].chr
+    return a[0].to_i.chr + a[1].to_i.chr + a[2].to_i.chr + a[3].to_i.chr
   end
 
   private
